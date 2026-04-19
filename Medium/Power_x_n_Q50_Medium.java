def847cca78db3da5eaccd721564d1bac128b320
Lojana sarma Saravanabhava sarma <lojanasarma@gmail.com>
2026-04-19 19:17:40 +0530

    Add files via upload
    Power(x,n) Question 50 Medium
    Link : https://leetcode.com/problems/powx-n/description/

---
+++ Medium/Power_x_n_Q50_Medium.java
@@ -0,0 +1,18 @@
+
+package Power_x_n_Q50_Medium;
+
+/*
+ * In Java, the built-in method Math.pow(x, n) is used
+ * to calculate the power of a number.
+ *
+ * It returns x raised to the power n (x^n).
+ */
+class Solution {
+    public double myPow(double x, int n) {
+
+        double result = Math.pow(x,n);
+
+        return result;
+
+    }
+}
